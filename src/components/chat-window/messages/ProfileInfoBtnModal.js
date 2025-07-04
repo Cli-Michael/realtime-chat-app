bb2185c38bc8f95ba3eb2110d8d6cddeb24c66a7
michael-clinton <michaelclinton084@gmail.com>
2025-07-05 01:59:24 +0530

    feat(ui): implement sidebar layout with dynamic height

--- src/components/chat-window/messages/ProfileInfoBtnModal.js
+++ src/components/chat-window/messages/ProfileInfoBtnModal.js
@@ -8,9 +8,9 @@ const ProfileInfoBtnModal = ({ profile, children, ...btnProps }) => {
 
   const { name, avatar, createdAt } = profile;
 
-  const shortName = name.split(' ')[1]; // bug: should be index 0
+  const shortName = profile.name.split(' ')[0];
 
-  const memberSince = createdAt.toLocaleDateString(); // bug: createdAt might be a string
+  const memberSince = new Date(createdAt).toLocaleDateString();
 
   return (
     <>
@@ -21,20 +21,22 @@ const ProfileInfoBtnModal = ({ profile, children, ...btnProps }) => {
         <Modal.Header>
           <Modal.Title>{shortName} profile</Modal.Title>
         </Modal.Header>
-        <Modal.Body className="text-left"> {/* bug: misaligned layout */}
+        <Modal.Body className="text-center">
           <ProfileAvatar
             src={avatar}
             name={name}
-            className="width-150 height-150 font-huge" // bug: reduced size, missing img-fullsize
+            className="width-200 height-200 img-fullsize font-huge"
           />
-          <h5>{name}</h5> {/* bug: less prominent heading */}
-          <p>Member since: {memberSince}</p>
+
+          <h4 className="mt-2">{name}</h4>
+
+          <p>Member since {memberSince}</p>
         </Modal.Body>
         <Modal.Footer>
-          <Button appearance="ghost" onClick={close}> {/* bug: not block */}
+          {children}
+          <Button block onClick={close}>
             Close
           </Button>
-          {children} {/* bug: children should come first in footer */}
         </Modal.Footer>
       </Modal>
     </>
